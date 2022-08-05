8d29bfe561ea95a178fb2919bed0d0c58db05ce8
10dy10 <kdy93102@hanmail.net>
2022-08-05 15:17:27 +0900

    push

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <TheLoading v-if="isLoading"/>
   <TheHeader />
   <div class="wrap">
     <TheMain />
@@ -6,6 +7,7 @@
 </template>
 
 <script>
+import TheLoading from './components/TheLoading.vue'
 import TheHeader from './layouts/TheHeader.vue'
 import TheMain from './components/TheMain.vue'
 
@@ -14,17 +16,31 @@ export default {
   components: {
     TheHeader,
     TheMain,
+    TheLoading
   },
   data(){
     return {
-
+      isLoading: true,
     }
+  },
+  methods: {
+    // async fetchData(){
+    //   this.isLoading = true;
+    //   const { data } = await fetchPost();
+    //   this.isLoading = false;
+    //   console.log(data.posts);
+    //   this.postItems = data.posts;
+    // }
+  },
+  created(){
+    // this.fetchData();
   }
 }
 </script>
 
 <style lang="scss">
   #app {
+    width: 100%;
     color: $text;
     background-color: $white;
   }
--- src/components/TheLoading.vue
+++ src/components/TheLoading.vue
@@ -1,70 +1,104 @@
 <template>
-  <div class="container">
-    <div class="circle"></div>
-    <div class="square"></div>
-    <div class="triangle"></div>
-    <div class="logo">DAYOUNG</div>
-    <div class="shadow"></div>
+  <div class="loading">
+    <div class="container">
+      <div class="circle"></div>
+      <div class="square"></div>
+      <div class="triangle"></div>
+      <!-- <div class="logo">DAYOUNG</div> -->
+      <div class="shadow"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
 
-}
+};
 </script>
 
 <style lang="scss" scope>
-@import url('https://fonts.googleapis.com/css?family=Raleway:200');
+@import url("https://fonts.googleapis.com/css?family=Raleway:200");
 
-.container {
+.loading {
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100vh;
-  font-family: 'Raleway', sans-serif;
-  .shadow {
-    width: 60px;
-    height: 4px;
-    background-color: rgba(0, 0, 0, .1);
-    border-radius: 3px;
-    animation: shadow 5s ease-in-out infinite;
-    margin: auto;
+  background: $white;
+  .container {
     position: absolute;
-    bottom: -4px;
-    left: 0;
-    right: 0;
-  }
-}
-
-@keyframes shadow {
-  0% {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
     width: 60px;
-  }
-  10% {
-    width: 10px;
-  }
-  20% {
-    width: 60px;
-  }
-  30%{
-    width:10px;
-  }
-  40%{
-    width:60px;
-  }
-  50%{
-    width:10px;
-  }
-  60%{
-    width:60px;
-  }
-  70%{
-    width:10px;
-  }
-  92%{
-    width:10px;
-  }
-  100%{
-    width:60px;
+    height: 250px;
+    margin: auto;
+    font-family: "Raleway", sans-serif;
+    .circle {
+      width: 55px;
+      height: 55px;
+      border: 2px solid #2ecc71;
+      background-color: #ffffff;
+      border-radius: 50%;
+      position: absolute;
+      bottom: 0;
+      animation: circle 5s ease-in-out infinite;
+    }
+    .square {
+      width: 55px;
+      height: 55px;
+      border: 2px solid #f1c40f;
+      background-color: #ffffff;
+      position: absolute;
+      bottom: 0;
+      opacity: 0;
+      animation: square 5s ease-in-out infinite;
+    }
+    .triangle {
+      width: 0;
+      height: 0;
+      border-left: 30px solid transparent;
+      border-right: 30px solid transparent;
+      border-bottom: 50px solid #9b59b6;
+      position: absolute;
+      bottom: 0;
+      animation: triangle 5s ease-in-out infinite;
+      opacity: 0;
+    }
+    .triangle::after {
+      content: "";
+      width: 0;
+      height: 0;
+      border-left: 26px solid transparent;
+      border-right: 26px solid transparent;
+      border-bottom: 44px solid white;
+      position: absolute;
+      bottom: -48px;
+      left: -26px;
+    }
+    .logo {
+      position: absolute;
+      bottom: 0;
+      text-align: center;
+      color: #3498db;
+      font-size: 30px;
+      margin-left: -5px;
+      opacity: 0;
+      animation: logo 5s ease-in-out infinite;
+    }
+    .shadow {
+      width: 60px;
+      height: 4px;
+      background-color: rgba(0, 0, 0, 0.1);
+      border-radius: 3px;
+      animation: shadow 5s ease-in-out infinite;
+      margin: auto;
+      position: absolute;
+      bottom: -4px;
+      left: 0;
+      right: 0;
+    }
   }
 }
 </style>
